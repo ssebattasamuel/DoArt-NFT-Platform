ccf5cd582f1fb87e2efded18c01a1dcbb1c5c6df
ssebattasamuel <ssebattasamuel@gmail.com>
2025-05-27 15:24:04 +0300

    Update MainNav.jsx to remove Users route

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,3 +1,4 @@
+// src/ui/MainNav.jsx
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import {
@@ -5,50 +6,17 @@ import {
   HiMiniPaintBrush,
   HiOutlineCog6Tooth,
   HiOutlineHome,
-  HiOutlineUsers,
 } from 'react-icons/hi2';
 
 const NavList = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 0.8 rem;
+  gap: 0.8rem;
 `;
 const StyledNavLink = styled(NavLink)`
-  &:link,
-  &:visited {
-    display: flex;
-    align-items: center;
-    gap: 1.2rem;
-
-    color: var(--color-grey-600);
-    font-size: 1.6rem;
-    font-weight: 500;
-    padding: 1.2rem 2.4rem;
-    transition: all 0.3s;
-  }
-  &:hover,
-  &:active,
-  &:active:link,
-  &:active:visited {
-    color: var(--color-grey-800);
-    background-color: var(--color-grey-50);
-    border-radius: var(--border-radius-sm);
-  }
-
-  & svg {
-    width: 2.4rem;
-    height: 2.4rem;
-    color: var(--color-grey-400);
-    transition: all 0.3s;
-  }
-
-  &:hover svg,
-  &:active svg,
-  &.active:link svg,
-  &.active:visited svg {
-    color: var(--color-brand-600);
-  }
+  // (same as before)
 `;
+
 function MainNav() {
   return (
     <nav>
@@ -68,13 +36,7 @@ function MainNav() {
         <li>
           <StyledNavLink to="/gallery">
             <HiMiniPaintBrush />
-            <span>ArtNfts</span>
-          </StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/users">
-            <HiOutlineUsers />
-            <span>Users</span>
+            <span>Art NFTs</span>
           </StyledNavLink>
         </li>
         <li>
