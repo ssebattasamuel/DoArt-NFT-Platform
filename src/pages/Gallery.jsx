9f23ac5f0e1abb96ade8e445d16961788fcab739
ssebattasamuel <ssebattasamuel@gmail.com>
2025-05-27 14:59:14 +0300

    Update Gallery.jsx to use ui/ components and pass provider/signer

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,45 +1,19 @@
-// import ArtNftContainer from '../data/features/ArtNftContainer';
-// import Heading from '../ui/Heading';
-// import Row from '../ui/Row';
-// import AddNft from '../data/features/AddNft';
-
-// function Gallery() {
-//   return (
-//     <>
-//       <Row type="horizontal">
-//         <Heading as="h1">all art</Heading>
-//         <p>Filter/sort</p>
-//       </Row>
-//       <Row>
-//         <ArtNftContainer />
-//         <AddNft />
-//       </Row>
-//     </>
-//   );
-// }
-// export default Gallery;
-
-import ArtNftContainer from '../data/features/ArtNftContainer';
+// src/pages/Gallery.jsx
+import ArtNftContainer from '../ui/ArtNftContainer-v1';
+import AddNft from '../ui/AddNft';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import AddNft from '../data/features/AddNft';
 
-
-import CreateNftForm from '../data/features/CreateNftForm';
-
-function Gallery() {
-  
-
+function Gallery({ provider, signer }) {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All Art</Heading>
-        <p>Filter/Sort</p> 
+        <p>Filter/Sort (Coming Soon)</p>
       </Row>
       <Row>
-        <ArtNftContainer />
-       <AddNft/>
-        
+        <AddNft />
+        <ArtNftContainer provider={provider} signer={signer} />
       </Row>
     </>
   );
