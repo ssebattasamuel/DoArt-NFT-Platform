9d179c67b8d51ac3a1b4f6900da30f6a7a38ea81
ssebattasamuel <ssebattasamuel@gmail.com>
2025-05-27 15:48:52 +0300

    Update useDeleteNft to cancel listings instead of deleting

--- src/hooks/useDeleteNft.js
+++ src/hooks/useDeleteNft.js
@@ -1,18 +1,16 @@
+// src/hooks/useDeleteNft.js
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
-import { deleteNft as deleteNftApi } from '../../services/apiArtNfts';
+import { cancelListing } from '../services/apiArtNfts';
 
 export function useDeleteNft() {
   const queryClient = useQueryClient();
 
   const { isLoading: isDeleting, mutate: deleteNft } = useMutation({
-    mutationFn: deleteNftApi,
+    mutationFn: cancelListing,
     onSuccess: () => {
-      toast.success('Cabin successfully deleted');
-
-      QueryClient.invalidateQueries({
-        queryKey: ['cabins'],
-      });
+      toast.success('Listing successfully canceled');
+      queryClient.invalidateQueries({ queryKey: ['artnfts'] });
     },
     onError: (err) => toast.error(err.message),
   });
