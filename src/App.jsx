f6b59a0806f883ccc230758b5e40e1719252d9fc
ssebattasamuel <ssebattasamuel@gmail.com>
2025-05-28 08:10:33 +0300

    Fix AppLayout import path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 import GlobalStyles from './styles/GlobalStyles';
-import AppLayout from './components/AppLayout';
+import AppLayout from './ui/AppLayout';
 import Dashboard from './pages/Dashboard';
 import Gallery from './pages/Gallery';
 import Minting from './pages/Minting';
