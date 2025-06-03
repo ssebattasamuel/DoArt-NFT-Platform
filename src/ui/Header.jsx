5e888145582be016652bb1f84377c908348e50ea
ssebattasamuel <ssebattasamuel@gmail.com>
2025-06-03 22:10:12 +0300

    Modified header to INclude navigation

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,16 @@
-// src/ui/Header.jsx
 import styled from 'styled-components';
 import { ethers } from 'ethers';
 import { useState } from 'react';
 import { toast } from 'react-hot-toast';
+import { NavLink } from 'react-router-dom';
 import Button from './Button';
 import Logo from './Logo';
+import {
+  HiOutlineHome,
+  HiMiniBanknotes,
+  HiMiniPaintBrush,
+  HiOutlineCog6Tooth
+} from 'react-icons/hi2';
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
@@ -15,9 +21,25 @@ const StyledHeader = styled.header`
   align-items: center;
 `;
 
+const NavList = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 2rem;
+  align-items: center;
+`;
+
+const NavItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
 const WalletInfo = styled.div`
   font-size: 1.4rem;
   color: var(--color-grey-600);
+  display: flex;
+  align-items: center;
+  gap: 1rem;
 `;
 
 function Header({ setProvider, setSigner, setAccount, account }) {
@@ -46,15 +68,61 @@ function Header({ setProvider, setSigner, setAccount, account }) {
   return (
     <StyledHeader>
       <Logo />
+      <NavList>
+        <NavItem>
+          <NavLink
+            to="/dashboard"
+            style={({ isActive }) => ({
+              color: isActive
+                ? 'var(--color-brand-600)'
+                : 'var(--color-grey-600)'
+            })}
+          >
+            <HiOutlineHome /> Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            to="/trades"
+            style={({ isActive }) => ({
+              color: isActive
+                ? 'var(--color-brand-600)'
+                : 'var(--color-grey-600)'
+            })}
+          >
+            <HiMiniBanknotes /> Trades
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            to="/gallery"
+            style={({ isActive }) => ({
+              color: isActive
+                ? 'var(--color-brand-600)'
+                : 'var(--color-grey-600)'
+            })}
+          >
+            <HiMiniPaintBrush /> Art NFTs
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            to="/settings"
+            style={({ isActive }) => ({
+              color: isActive
+                ? 'var(--color-brand-600)'
+                : 'var(--color-grey-600)'
+            })}
+          >
+            <HiOutlineCog6Tooth /> Settings
+          </NavLink>
+        </NavItem>
+      </NavList>
       <WalletInfo>
         {account ? (
           `Connected: ${account.slice(0, 6)}...${account.slice(-4)}`
         ) : (
-          <Button onClick={connectWallet}>
-            {account
-              ? `${account.slice(0, 6)}...${account.slice(-4)}`
-              : 'Connect Wallet'}
-          </Button>
+          <Button onClick={connectWallet}>Connect Wallet</Button>
         )}
       </WalletInfo>
     </StyledHeader>
